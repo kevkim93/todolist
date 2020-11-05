f72e20b1416d3890d102ec04b46e4944f8a45e25
kevkim93 <70291064+kevkim93@users.noreply.github.com>
2020-11-05 11:05:16 -0600

    V2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-var tasks = [];
 
 $(document).ready(() => {
     // Updating task list when a new entry comes in
@@ -13,17 +12,14 @@ $(document).ready(() => {
                 content: $("input").first().val(),
                 idtask: Date.now()
             };
-            tasks.push(task);
-            $("#task-form")[0].reset();
-            update(tasks)
+            update(task)
+            $("#task-form")[0].reset(); 
         }
-        
     })
 
     $("#task-list").on("click", ".trash-button", deleteTask)
 
     $("#task-list").on("click", ".check-button", doneTask)
-
 });
 
 // Initiate list buttons
@@ -32,41 +28,36 @@ var checked_box = '<button class="check-button" type="button"><i class="far fa-c
 var trash = '<button class="trash-button" type="button"><i class="far fa-trash-alt" id="trash-button-id"></i></button>'
 
 // updating list function
-function update(array) {
-    document.getElementById('list').innerHTML = '';
-    var frag = document.createDocumentFragment();
-    array.forEach((task,index) => {
-        var li = document.createElement('li');
-        li.setAttribute('id',('list-item'+index));
-        
-        if (task.checked === 0){
-            li.innerHTML = unchecked_box+`<span id="${task.idtask}">`+task.content+'</span>'+trash;
-            li.setAttribute('class',(`checked${task.checked}`))
-        } else if (task.checked === 1){
-            li.innerHTML = checked_box+`<span id="${task.idtask}">`+task.content+'</span>'+trash;
-            li.setAttribute('class',(`checked${task.checked}`))
-        }
-        frag.appendChild(li);
-    }); 
-    document.getElementById('list').appendChild(frag)
+function update(task) {
+    var li = document.createElement('li');
+    li.setAttribute('id',('list-item'+task.idtask))
+    if (task.checked === 0){
+        li.innerHTML = unchecked_box+`<span id="${task.idtask}">`+task.content+'</span>'+trash;
+        li.setAttribute('class',(`checked${task.checked}`))
+    } else if (task.checked === 1){
+        li.innerHTML = checked_box+`<span id="${task.idtask}">`+task.content+'</span>'+trash;
+        li.setAttribute('class',(`checked${task.checked}`))
+    }
+    document.getElementById('list').appendChild(li);
 }
 
 // delete task in array tasks if the content matches the text next to the delete-button. 
 function deleteTask() {
-    let id = this.parentElement.getAttribute('id').split('list-item')[1]
-    tasks.splice(id,1);
-    update(tasks)
+    var id = this.parentElement.getAttribute('id');
+    var listitem = document.getElementById(id);
+    listitem.remove();
 }
 
 // updating task.checked values for checkbox icons
 function doneTask() {
-    tasks.forEach(task => {
-        if(this.parentElement.childNodes[1].getAttribute('id') == task.idtask)   {
-            if(task.checked === 1){
-                task.checked = 0;
-            } else if(task.checked === 0){
-                task.checked = 1;
-            }
-    }})
-    update(tasks)
+    let checked = this.parentElement.getAttribute('class').split('checked')[1]
+    if (checked == 0) {
+        this.parentElement.setAttribute('class','checked1');
+        this.parentElement.innerHTML = this.parentElement.innerHTML.replace('<i class="far fa-square"', '<i class="far fa-check-square"')
+    }
+    
+    if (checked == 1) {
+        this.parentElement.setAttribute('class','checked0');
+        this.parentElement.innerHTML = this.parentElement.innerHTML.replace('<i class="far fa-check-square"','<i class="far fa-square"')
+    }
 }
