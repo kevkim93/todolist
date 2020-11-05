c0128da4e9b9b8bcfba9b414a708b335c8002769
kevkim93 <70291064+kevkim93@users.noreply.github.com>
2020-11-04 22:56:37 -0600

    V1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 
 var tasks = [];
-
 $(document).ready(() => {
     // Updating task list when a new entry comes in
     
     $("#submit-button").click((event)=>{
         event.preventDefault();
-        const task = { 
-            checked: 0,
-            content: $("input").first().val()
-        };
-        tasks.push(task);
-        $("#task-form")[0].reset();
-        update(tasks)
+        if ($("input").first().val() == ''){
+            alert('Must enter a task first')
+        }
+        else {
+            const task = { 
+                checked: 0,
+                content: $("input").first().val(),
+                idtask: Date.now()
+            };
+            tasks.push(task);
+            $("#task-form")[0].reset();
+            update(tasks)
+        }
+        
     })
+
+    $("#task-list").on("click", ".trash-button", deleteTask)
+
+
+    $("#task-list").on("click", ".check-button", doneTask)
+
 
     // These don't seem to get triggered so I moved onto onclick attributes in button tags
 
@@ -47,60 +59,55 @@ $(document).ready(() => {
 });
 
 // Initiate list buttons
-var unchecked_box = '<button class="check-button" type="button" onClick="doneTask()"><i class="far fa-square" id="check-button-id"></i></button>'
-var checked_box = '<button class="check-button" type="button" onClick="doneTask()"><i class="far fa-check-square" id="check-button-id"></i></button>'
-var trash = '<button class="trash-button" type="button" onClick="deleteTask()"><i class="far fa-trash-alt" id="trash-button-id"></i></button>'
+var unchecked_box = '<button class="check-button" type="button"><i class="far fa-square" id="check-button-id"></i></button>'
+var checked_box = '<button class="check-button" type="button"><i class="far fa-check-square" id="check-button-id"></i></button>'
+var trash = '<button class="trash-button" type="button"><i class="far fa-trash-alt" id="trash-button-id"></i></button>'
 
 // updating list function
 function update(array) {
-    document.getElementById('task-list').innerHTML = '';
+    document.getElementById('list').innerHTML = '';
     var frag = document.createDocumentFragment();
     array.forEach((task,index) => {
         var li = document.createElement('li');
-        li.setAttribute('id',('list-item'+index))
+        li.setAttribute('id',('list-item'+index));
         
         if (task.checked === 0){
-            li.innerHTML = unchecked_box+'<span>'+task.content+'</span>'+trash;
+            li.innerHTML = unchecked_box+`<span id="${task.idtask}">`+task.content+'</span>'+trash;
+            li.setAttribute('class',(`checked${task.checked}`))
             // str+= `<li class="list-item" id="${'list-item'+index}">`+unchecked_box+task.content+trash+'</li>';
         } else if (task.checked === 1){
-            li.innerHTML = checked_box+'<span>'+task.content+'</span>'+trash;
+            li.innerHTML = checked_box+`<span id="${task.idtask}">`+task.content+'</span>'+trash;
+            li.setAttribute('class',(`checked${task.checked}`))
             // str+= `<li class="list-item" id="${'list-item'+index}">`+checked_box+task.content+trash+'</li>';
         }
         // alert(li.textContent);
         // alert(array)
         frag.appendChild(li);
     }); 
-    document.getElementById('task-list').appendChild(frag)
+    document.getElementById('list').appendChild(frag)
 }
 
 
 // delete task in array tasks if the content matches the text next to the delete-button. 
 function deleteTask() {
-    // alert($(this).closest("li").prop('#text'))
-    
-    tasks = $.grep(tasks,(el) => {
-        // alert(el.content)
-        var textCont = $(this).siblings('span').text();
-        alert(textCont)
-        return el.content != $(this).closest("li").html();
-        // event.target.closest("li").html()
-    })
+    let id = this.parentElement.getAttribute('id').split('list-item')[1]
+    tasks.splice(id,1);
+    // tasks = $.grep(tasks,(el) => {
+    //     return el.content != $(this).closest("li").html();
+    //     // event.target.closest("li").html()
+    // })
     update(tasks)
 }
 
 // updating task.checked values for checkbox icons
 // todo: get id of the list, $(listID).text() == task.content 
 function doneTask() {
-    var listID = $(this).closest("li").prop('id') 
-    // var listID = $(this).siblings("span").text() 
-    alert(listID)
     tasks.forEach(task => {
-        // alert(task.checked)
-        if(this.closest("#list-item").text() == task.content) {
+        if(this.parentElement.childNodes[1].getAttribute('id') == task.idtask)   {
             if(task.checked === 1){
-                task.checked = 0
+                task.checked = 0;
             } else if(task.checked === 0){
-                task.checked = 1
+                task.checked = 1;
             }
     }})
     update(tasks)
